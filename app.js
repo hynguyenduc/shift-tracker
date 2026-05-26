f133a8ce502fec6196ca7a6b34b55c31d0ef4554
taijitsuhitsyou <hy.nguyenduc452@gmail.com>
2026-05-26 11:22:02 +1000

    added function to calculate date

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const tabButtons = document.querySelectorAll('nav button')
 const tabSections = document.querySelectorAll('section')
+const monthNames = [
+  'January', 'February', 'March', 'April',
+  'May', 'June', 'July', 'August',
+  'September', 'October', 'November', 'December'
+]
+let currentMonth = new Date().getMonth()
+let currentYear = new Date().getFullYear()
 
 function switchTab(tabName) {
     tabSections.forEach(function(section) {
@@ -11,6 +18,15 @@ function switchTab(tabName) {
     document.querySelector('#' + tabName + '-view').classList.remove('hidden')
     
     document.querySelector('[data-tab="' + tabName + '"]').classList.add('tab-active')
+}
+
+function renderCalendar() {
+    let firstDay = new Date(currentYear, currentMonth, 1).getDay()
+    let totalDays = new Date(currentYear, currentMonth+1, 0).getDate()
+    let monthLabel = monthNames[currentMonth] + ' ' + currentYear
+
+    document.querySelector('#month-label').textContent = monthLabel
+    document.querySelector('#cal-grid').innerHTML = ''
 }
 
 tabButtons.forEach(function(button) {
